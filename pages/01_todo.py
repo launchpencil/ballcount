f9b99251c3aa82353ef49d1f92557b439624880e
launchpencil <launchpencil@gmail.com>
2023-05-03 17:50:42 +0900

    add todo

--- pages/01_todo.py
+++ pages/01_todo.py
@@ -1,26 +1,21 @@
 import streamlit as st
 import requests as r
 
-st.title('hello world!')
+st.title('todo')
 
 with st.form(key='login_form'):
     username = st.text_input('ユーザー名')
     password = st.text_input('パスワード', type='password')
-    submit_button = st.form_submit_button(label='ログイン')
+    submit_button = st.form_submit_button(label='表示')
 
 if submit_button:
-    response = r.get(f'https://api.launchpencil.f5.si/zikanwari/?user={username}&pass={password}')
+    response = r.get(f'https://api.launchpencil.f5.si/todo/?user={username}&pass={password}')
     if response.status_code == 200:
         
         a = response.text.split(',')
 
-        table_data = [[0 for j in range(5)] for i in range(7)]
-        table_data[1][2] = 5
-
-        for i in range(0, 35):
-            table_data[i//5][i%5] = a[i]
-
-        st.table(table_data)
+        for i in range(len(a) -1):
+            st.markdown('* ' + a[i])
     else:
         # エラーが発生した場合はエラーメッセージを表示する
         print("Error: {}".format(response.status_code))
